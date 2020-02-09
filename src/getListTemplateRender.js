2dbc7a12d9af62219eb28c001b49979937f7249d
hqweay <hqweay@gmail.com>
2020-02-09 16:37:44 +0800

    add: 给展示页面添加目录

--- src/getListTemplateRender.js
+++ src/getListTemplateRender.js
@@ -1,13 +1,21 @@
 const { DoubanTypeEnum } = require('markall-douban-backup');
 
-let { movieTemplateRender } = require('./itemTemplateRender');
+let { movieTemplateRender, bookTemplateRender, musicTemplateRender, gameTemplateRender } = require('./itemTemplateRender');
 
 
 function getListTemplateRender(type, data) {
   let listHtml = '<div class="hexo-douban-getter-movies">';
+  listHtml += `<h1>${type}</h1>`;
   for (let index in data) {
+    listHtml += `<h2>${data[index].title || data[index].name}</h2 > `;
     if (type === DoubanTypeEnum.watchedMovies || type === DoubanTypeEnum.wishMovies || type === DoubanTypeEnum.watchingMovies) {
       listHtml += movieTemplateRender(data[index]);
+    } else if (type === DoubanTypeEnum.readBooks || type === DoubanTypeEnum.wishBooks || type === DoubanTypeEnum.readingBooks) {
+      listHtml += bookTemplateRender(data[index]);
+    } else if (type === DoubanTypeEnum.listenedMusics || type === DoubanTypeEnum.wishMusics || type === DoubanTypeEnum.listeningMusics) {
+      listHtml += musicTemplateRender(data[index]);
+    } else if (type === DoubanTypeEnum.playedGames || type === DoubanTypeEnum.wishGames || type === DoubanTypeEnum.playingGames) {
+      listHtml += gameTemplateRender(data[index]);
     }
   }
   listHtml += "</div>";
--- src/itemTemplateRender.js
+++ src/itemTemplateRender.js
@@ -1,13 +1,11 @@
 function movieTemplateRender(item) {
   return `
     <div class="hexo-douban-movie" style="position:relative;width:60%;border:1px solid #666;border-radius: 4px;margin:10px auto;padding: 10px;"> 
-    
       <div class ="pic" style="width: 50%;">
        <img src="${item.pic}" onerror="this.style.display='none'"
         style="width: 135px; height: 192px; display:inline-block; margin-right: 14px;"  referrerpolicy="no-referrer" 
        ></img>
       </div>
-
       <div class ="title">
         <a href="${item.url}" target="_blank">
         ${item.name}
@@ -20,28 +18,90 @@ function movieTemplateRender(item) {
         标签：${item.oldTags}
       </div>
     </div>`;
-  // return `
-  //   <div class="hexo-douban-movie" style="display: flex;"> 
-
-  //   <div class ="pic" style="width: 50%;"">
-  //     <img src="${item.pic}" onerror="this.src='https://leay.net/img/avatar.png'" ></img>
-  //   </div>
-
-  //   <div class = "content" style="align-items:center;">
-  //     <div class ="title">
-  //       <a href="${item.url}" target="_blank">
-  //         ${item.name}
-  //       </a>
-  //     </div>
-  //     <div class ="comment">
-  //       评论：${item.comment}
-  //     </div>
-  //     <div class ="tags">
-  //      标签：${item.oldTags}
-  //     </div>
-
-  //     </div>
-  //   </div>`;
 }
 
-module.exports = { movieTemplateRender };
+function bookTemplateRender(item) {
+  return `
+    <div class="hexo-douban-movie" style="position:relative;width:60%;border:1px solid #666;border-radius: 4px;margin:10px auto;padding: 10px;"> 
+      <div class ="pic" style="width: 50%;">
+       <img src="${item.pic}" onerror="this.style.display='none'"
+        style="width: 135px; height: 192px; display:inline-block; margin-right: 14px;"  referrerpolicy="no-referrer" 
+       ></img>
+      </div>
+      <div class ="title">
+        <a href="${item.url}" target="_blank">
+        ${item.title}
+        </a>
+      </div>
+       <div class ="pub">
+        出版信息：${item.pub}
+      </div>
+       <div class ="date">
+        标记时间：${item.date}
+      </div>
+      <div class ="comment">
+        评论：${item.comment}
+      </div>
+      <div class ="tags">
+        标签：${item.oldTags}
+      </div>
+    </div>`;
+}
+
+function musicTemplateRender(item) {
+  return `
+    <div class="hexo-douban-movie" style="position:relative;width:60%;border:1px solid #666;border-radius: 4px;margin:10px auto;padding: 10px;"> 
+      <div class ="pic" style="width: 50%;">
+       <img src="${item.pic}" onerror="this.style.display='none'"
+        style="width: 135px; height: 192px; display:inline-block; margin-right: 14px;"  referrerpolicy="no-referrer" 
+       ></img>
+      </div>
+      <div class ="title">
+        <a href="${item.url}" target="_blank">
+        ${item.title}
+        </a>
+      </div>
+       <div class ="intro">
+        介绍：${item.intro}
+      </div>
+       <div class ="date">
+        标记时间：${item.date}
+      </div>
+      <div class ="comment">
+        评论：${item.comment}
+      </div>
+      <div class ="tags">
+        标签：${item.oldTags}
+      </div>
+    </div>`;
+}
+
+function gameTemplateRender(item) {
+  return `
+    <div class="hexo-douban-movie" style="position:relative;width:60%;border:1px solid #666;border-radius: 4px;margin:10px auto;padding: 10px;"> 
+      <div class ="pic" style="width: 50%;">
+       <img src="${item.pic}" onerror="this.style.display='none'"
+        style="width: 135px; height: 192px; display:inline-block; margin-right: 14px;"  referrerpolicy="no-referrer" 
+       ></img>
+      </div>
+      <div class ="title">
+        <a href="${item.url}" target="_blank">
+        ${item.title}
+        </a>
+      </div>
+       <div class ="intro">
+        介绍：${item.intro}
+      </div>
+       <div class ="date">
+        标记时间：${item.date}
+      </div>
+      <div class ="comment">
+        评论：${item.comment}
+      </div>
+      <div class ="tags">
+        标签：${item.oldTags}
+      </div>
+    </div>`;
+}
+
+module.exports = { movieTemplateRender, bookTemplateRender, musicTemplateRender, gameTemplateRender };
