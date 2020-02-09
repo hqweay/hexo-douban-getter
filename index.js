dd6526a150ff3be81ea6b083e5cffb3fdbc45dcd
hqweay <hqweay@gmail.com>
2020-02-09 16:55:43 +0800

    add: 根据分页获取信息

--- index.js
+++ index.js
@@ -12,73 +12,73 @@ hexo.extend.tag.register('douban', function (args) {
   switch (type) {
     case DoubanTypeEnum.watchedMovies:
       let getDoubanWatchedMovies = createDoubanDataGetter(DoubanTypeEnum.watchedMovies);
-      return getDoubanWatchedMovies(userName, pageStart = 1, pageEnd = 1).then(function (data) {
+      return getDoubanWatchedMovies(userName, pageStart, pageEnd).then(function (data) {
         return getListTemplateRender(type, data);
       })
       break;
     case DoubanTypeEnum.wishMovies:
       let getDoubanWishMovies = createDoubanDataGetter(DoubanTypeEnum.wishMovies);
-      return getDoubanWishMovies(userName, pageStart = 1, pageEnd = 1).then(function (data) {
+      return getDoubanWishMovies(userName, pageStart, pageEnd).then(function (data) {
         return getListTemplateRender(type, data);
       })
       break;
     case DoubanTypeEnum.watchingMovies:
       let getDoubanWatchingMovies = createDoubanDataGetter(DoubanTypeEnum.watchingMovies);
-      return getDoubanWatchingMovies(userName, pageStart = 1, pageEnd = 1).then(function (data) {
+      return getDoubanWatchingMovies(userName, pageStart, pageEnd).then(function (data) {
         return getListTemplateRender(type, data);
       })
       break;
     case DoubanTypeEnum.readBooks:
       let getDoubanReadBooks = createDoubanDataGetter(DoubanTypeEnum.readBooks);
-      return getDoubanReadBooks(userName, pageStart = 1, pageEnd = 1).then(function (data) {
+      return getDoubanReadBooks(userName, pageStart, pageEnd).then(function (data) {
         return getListTemplateRender(type, data);
       })
       break;
     case DoubanTypeEnum.wishBooks:
       let getDoubanWishBooks = createDoubanDataGetter(DoubanTypeEnum.wishBooks);
-      return getDoubanWishBooks(userName, pageStart = 1, pageEnd = 1).then(function (data) {
+      return getDoubanWishBooks(userName, pageStart, pageEnd).then(function (data) {
         return getListTemplateRender(type, data);
       })
       break;
     case DoubanTypeEnum.readingBooks:
       let getDoubanReadingBooks = createDoubanDataGetter(DoubanTypeEnum.readingBooks);
-      return getDoubanReadingBooks(userName, pageStart = 1, pageEnd = 1).then(function (data) {
+      return getDoubanReadingBooks(userName, pageStart, pageEnd).then(function (data) {
         return getListTemplateRender(type, data);
       })
       break;
     case DoubanTypeEnum.listenedMusics:
       let getDoubanListenedMusics = createDoubanDataGetter(DoubanTypeEnum.listenedMusics);
-      return getDoubanListenedMusics(userName, pageStart = 1, pageEnd = 1).then(function (data) {
+      return getDoubanListenedMusics(userName, pageStart, pageEnd).then(function (data) {
         return getListTemplateRender(type, data);
       })
       break;
     case DoubanTypeEnum.wishMusics:
       let getDoubanWishMusics = createDoubanDataGetter(DoubanTypeEnum.wishMusics);
-      return getDoubanWishMusics(userName, pageStart = 1, pageEnd = 1).then(function (data) {
+      return getDoubanWishMusics(userName, pageStart, pageEnd).then(function (data) {
         return getListTemplateRender(type, data);
       })
       break;
     case DoubanTypeEnum.listeningMusics:
       let getDoubanListeningMusics = createDoubanDataGetter(DoubanTypeEnum.listeningMusics);
-      return getDoubanListeningMusics(userName, pageStart = 1, pageEnd = 1).then(function (data) {
+      return getDoubanListeningMusics(userName, pageStart, pageEnd).then(function (data) {
         return getListTemplateRender(type, data);
       })
       break;
     case DoubanTypeEnum.playedGames:
       let getDoubanPlayedGames = createDoubanDataGetter(DoubanTypeEnum.playedGames);
-      return getDoubanPlayedGames(userName, pageStart = 1, pageEnd = 1).then(function (data) {
+      return getDoubanPlayedGames(userName, pageStart, pageEnd).then(function (data) {
         return getListTemplateRender(type, data);
       })
       break;
     case DoubanTypeEnum.wishGames:
       let getDoubanWishGames = createDoubanDataGetter(DoubanTypeEnum.wishGames);
-      return getDoubanWishGames(userName, pageStart = 1, pageEnd = 1).then(function (data) {
+      return getDoubanWishGames(userName, pageStart, pageEnd).then(function (data) {
         return getListTemplateRender(type, data);
       })
       break;
     case DoubanTypeEnum.playingGames:
       let getDoubanPlayingGames = createDoubanDataGetter(DoubanTypeEnum.playingGames);
-      return getDoubanPlayingGames(userName, pageStart = 1, pageEnd = 1).then(function (data) {
+      return getDoubanPlayingGames(userName, pageStart, pageEnd).then(function (data) {
         return getListTemplateRender(type, data);
       })
       break;
